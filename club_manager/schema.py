fa52f8820f23657f9634c53dc78e655e07aab2f6
v15hv4 <vishva2912@gmail.com>
2021-08-22 21:48:53 +0530

    Hide deleted club details from the public

--- club_manager/schema.py
+++ club_manager/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from graphql import GraphQLError
 from graphql_jwt.decorators import superuser_required
 from club_manager.models import Club
 from club_manager.types import ClubType
@@ -14,7 +15,14 @@ class Query(graphene.ObjectType):
         return Club.objects.filter(state="active").order_by("name")
 
     def resolve_club(self, info, club_id):
-        return Club.objects.get(pk=club_id)
+        user = info.context.user
+        club = Club.objects.get(pk=club_id)
+
+        # don't show deleted club details to the public
+        if club.state != "active" and not user.is_superuser:
+            raise GraphQLError("You do not have permission to access this resource.")
+
+        return club
 
     # admin queries
     admin_clubs = graphene.List(ClubType)
