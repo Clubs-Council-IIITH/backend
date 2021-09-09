2f507a127a0de5178855924acae15761d1952ed3
v15hv4 <vishva2912@gmail.com>
2021-09-09 21:12:23 +0530

    Introduce event manager app

---
+++ event_manager/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from club_manager.models import Club
+from multiselectfield import MultiSelectField
+
+# possible event audiences
+AUDIENCE_LIST = [
+    ["none", "-"],
+    ["ug1", "UG 1"],
+    ["ug2", "UG 2"],
+    ["ug3", "UG 3"],
+    ["ugx", "UG 4+"],
+    ["pg", "PG"],
+    ["staff", "Staff"],
+    ["faculty", "Faculty"],
+]
+
+# possible event states
+EVENT_STATE_LIST = [
+    ["created", "CREATED"],
+    ["approved", "APPROVED"],
+    ["published", "PUBLISHED"],
+    ["scheduled", "SCHEDULED"],
+    ["completed", "COMPLETED"],
+    ["deleted", "DELETED"],
+]
+
+
+class Event(models.Model):
+    club = models.ForeignKey(Club, on_delete=models.CASCADE, blank=False, null=False)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    name = models.CharField(max_length=250, blank=False, null=False)
+    description = models.TextField(default="No description available.")
+    venue = models.TextField(default="-")
+    audience = MultiSelectField(choices=AUDIENCE_LIST)
+    state = models.CharField(max_length=50, choices=EVENT_STATE_LIST, default="created")
