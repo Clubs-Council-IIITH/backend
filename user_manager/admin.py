14859a8390059eb5736c923134d1af3ec112d651
v15hv4 <vishva2912@gmail.com>
2022-03-24 04:29:18 +0530

    Introduce User and Member models

---
+++ user_manager/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from user_manager.models import User
+
+admin.site.register(Event)
---
+++ user_manager/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from club_manager.models import Club
+
+
+class User(models.Model):
+    firstName = models.CharField(max_length=250, blank=False, null=False)
+    lastName = models.CharField(max_length=250, blank=False, null=False)
+    mail = models.EmailField(blank=False, null=False)
+    batch = models.CharField(max_length=25, blank=False, null=False)
+
+
+class Member(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE, blank=False, null=False)
+    role = models.CharField(max_length=100)
+    year = models.IntegerField(
+        default=datetime.now().year,
+        validators=[MaxValueValidator(3000), MinValueValidator(2000)],
+    )
