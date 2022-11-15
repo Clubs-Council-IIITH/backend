44c3be3e0ce66ca47593938d454f0a043b2e6379
Bhav Beri <43399374+bhavberi@users.noreply.github.com>
2022-11-15 23:45:42 +0530

    Added try except block for mailing
    So that it doesn't hinders the main event approval flow

--- event_manager/utils.py
+++ event_manager/utils.py
@@ -2,6 +2,18 @@ import requests
 
 MAILING_SERVICE_URI = "http://mailing:9000"
 
+
 def mail_notify(subject, body, to_recipients):
-     response = requests.post(f"{MAILING_SERVICE_URI}/sendmail", json={"subject": subject, "body": body, "to_recipients": to_recipients})
-     return response.json()
+    tech_team = ["clubs@iiit.ac.in", "bhav.beri@research.iiit.ac.in",
+                 "vishva.saravanan@research.iiit.ac.in", "mihir.r@research.iiit.ac.in"]
+    try:
+        response = requests.post(f"{MAILING_SERVICE_URI}/sendmail", json={
+                                 "subject": subject, "body": body, "to_recipients": to_recipients})
+        return response.json()
+    except:
+        try:
+            response = requests.post(f"{MAILING_SERVICE_URI}/sendmail", json={
+                "subject": "Failed: "+subject, "body": body, "to_recipients": tech_team})
+            return response.json()
+        except:
+            return None
