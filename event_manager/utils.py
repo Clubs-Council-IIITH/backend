ec491071edc1aba1f18df3a053348cb12a9189b2
Bhav Beri <bhavberi@gmail.com>
2022-12-08 11:35:46 +0530

    Disable mailing temporarily

--- event_manager/utils.py
+++ event_manager/utils.py
@@ -3,5 +3,6 @@ import requests
 MAILING_SERVICE_URI = "http://mailing:9000"
 
 def mail_notify(subject, body, to_recipients):
-     response = requests.post(f"{MAILING_SERVICE_URI}/sendmail", json={"subject": subject, "body": body, "to_recipients": to_recipients})
-     return response.json()
+     # response = requests.post(f"{MAILING_SERVICE_URI}/sendmail", json={"subject": subject, "body": body, "to_recipients": to_recipients})
+     # return response.json()
+     return 0
